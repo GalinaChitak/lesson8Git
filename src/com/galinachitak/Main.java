043cd82dd4b77f97e991a95905016c64e66c33df
Galina Chitak <gala199@list.ru>
2022-01-23 16:39:41 +0300

    Update test2

--- src/com/galinachitak/Main.java
+++ src/com/galinachitak/Main.java
@@ -3,7 +3,7 @@ package com.galinachitak;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         test();
         test2();
     }
@@ -22,9 +22,23 @@ public class Main {
         System.out.println("Всего " + coincidences + " совпадений");
 
     }
-    private static void test2(){
 
-    }
+    private static void test2() {
+        int sum = 0;
+        for (int ticket = 000001; ticket <= 999999; ticket++) {
+            int number1 = ticket / 100000;
+            int number2 = (ticket % 100000) / 10000;
+            int number3 = (ticket % 10000) / 1000;
+            int number4 = (ticket % 1000) / 100;
+            int number5 = (ticket % 100) / 10;
+            int number6 = ticket % 10;
 
+            if (number1 + number2 + number3 == number4 + number5 + number6) {
+                sum++;
+            }
+        }
+        System.out.println("Совпадений  " + sum);
     }
+}
+
 
