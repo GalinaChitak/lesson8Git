355f2908c544c3d43fc4345e8c8c5e932e3443d5
Galina Chitak <gala199@list.ru>
2022-01-23 16:36:38 +0300

    Update test

--- src/com/galinachitak/Main.java
+++ src/com/galinachitak/Main.java
@@ -8,5 +8,19 @@ public class Main {
     }
 
     private static void test() {
+        int coincidences = 0;
+        for (int hour = 1; hour <= 24; hour++) {
+            int minutes;
+            for (minutes = 0; minutes <= 59; minutes++) {
+                if (hour / 10 % 10 == minutes % 10 & hour % 10 == minutes / 10 % 10) {
+                    coincidences++;
+                }
+            }
+        }
+
+        System.out.println("Всего " + coincidences + " совпадений");
+
     }
-}
+
+    }
+
